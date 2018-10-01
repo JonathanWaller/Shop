4c0c6ae7a3eecee2c0ae16f80bdfbacdd00a1280
JonathanWaller <waller.j27@gmail.com>
2018-09-30 20:42:19 -0500

    listing products on page

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import { connect } from "react-redux";
 import { getStore } from "../../ducks/storeReducer";
+import "./Store.css";
 
 class Store extends Component {
   constructor() {
@@ -20,11 +21,15 @@ class Store extends Component {
         <div key={item.product_id}>
           <h3>{item.product_name}</h3>
           <div>${item.product_price}</div>
+          <img src={item.product_img} className="store_productImg" />
         </div>
       );
     });
     return (
       <div>
+        {this.props.isLoading ? (
+          <img src="https://payload345.cargocollective.com/1/18/582678/9219397/loading-ttcredesign.gif" />
+        ) : null}
         <p>Heyyy from the STORE</p>
         {storeItems}
       </div>
