ba6c4aa48a29c68f9f70554d2024e6b9eea80413
JonathanWaller <waller.j27@gmail.com>
2018-10-22 09:35:52 -0500

    small change, testing git

--- src/components/Confirmation/Confirmation.js
+++ src/components/Confirmation/Confirmation.js
@@ -4,7 +4,9 @@ function Confirmation() {
   return (
     <div>
       <h1>Confirmation</h1>
-      <div>Thanks for shopping with NAME. Your order has been confirmed.</div>
+      <div>
+        Thanks for shopping with Laavish. Your order has been confirmed.
+      </div>
     </div>
   );
 }
