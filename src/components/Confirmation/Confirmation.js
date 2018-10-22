4cd78657123e7eff081578182d9e7f4bc5087750
JonathanWaller <waller.j27@gmail.com>
2018-10-21 23:44:16 -0500

    small change, testing branch

--- src/components/Confirmation/Confirmation.js
+++ src/components/Confirmation/Confirmation.js
@@ -4,7 +4,10 @@ function Confirmation() {
   return (
     <div>
       <h1>Confirmation</h1>
-      <div>Thanks for shopping with NAME. Your order has been confirmed.</div>
+      <div>
+        Thanks for shopping with NAME. Your order has been confirmed. Will add
+        more to this...
+      </div>
     </div>
   );
 }
