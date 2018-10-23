e9edf93b3e673ffa36818a0c24da86b60cecc402
JonathanWaller <waller.j27@gmail.com>
2018-10-23 12:26:06 -0500

    cleaning up, added splash to store page

---
+++ src/components/Splash/Splash.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Splash.css";
+
+const Splash = () => {
+  return (
+    <div>
+      <div className="splashPhoto" />
+    </div>
+  );
+};
+
+export default Splash;
