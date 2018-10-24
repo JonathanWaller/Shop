bc37a9cb0e43084b0e5d84bd6601b707434c9773
JonathanWaller <waller.j27@gmail.com>
2018-10-23 19:25:08 -0500

    categories working correctly

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -3,6 +3,7 @@ import Splash from "../Splash/Splash";
 import axios from "axios";
 import { connect } from "react-redux";
 import { getCategory } from "../../ducks/storeReducer";
+import { Link } from "react-router-dom";
 
 class Category extends Component {
   constructor() {
@@ -18,10 +19,55 @@ class Category extends Component {
 
   render() {
     console.log(this.props);
+    let storeItems = this.props.storeReducer.items.map(item => {
+      return (
+        <div className="store_productWrapper" key={item.product_id}>
+          <Link to={`/product/${item.product_id}`}>
+            <div className="store_productText">
+              <h3>{item.product_name}</h3>
+              <div>${item.product_price}</div>
+              <img src={item.product_img} className="store_productImg" alt="" />
+            </div>
+          </Link>
+          <div className="store_addButtonWrapper">
+            <button
+              className="store_addButton"
+              onClick={e => {
+                this.props.addToCart(
+                  item.product_id,
+                  item.product_name,
+                  item.product_price,
+                  item.product_img,
+                  item.product_quantity,
+                  item.product_size,
+                  item.product_category
+                );
+                //   .then(() =>
+                //     toast.success("Added to cart", {
+                //       position: toast.POSITION.TOP_RIGHT
+                //     })
+                //   );
+              }}
+            >
+              Add To Cart
+            </button>
+          </div>
+        </div>
+      );
+    });
     return (
       <div>
         <Splash />
-        <p>hello from category</p>
+        <div className="store_wrapper">
+          {this.props.storeReducer.isLoading ? (
+            <img
+              src="https://payload345.cargocollective.com/1/18/582678/9219397/loading-ttcredesign.gif"
+              alt=""
+            />
+          ) : null}
+
+          {storeItems}
+        </div>
       </div>
     );
   }
@@ -33,4 +79,3 @@ export default connect(
   mapStateToProps,
   { getCategory }
 )(Category);
-// export default Category;
