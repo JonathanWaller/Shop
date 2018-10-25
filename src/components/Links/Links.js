f9f41073538345f815851a6d11d02f9762fe0053
JonathanWaller <waller.j27@gmail.com>
2018-10-25 10:06:14 -0500

    cleaning mobile style

--- src/components/Links/Links.js
+++ src/components/Links/Links.js
@@ -7,19 +7,29 @@ const Links = () => {
     <div className="store__linksWrapper">
       <div className="store__links">
         <Link to={"/category/shirt"}>
-          <div id="store__link">Shirts</div>
+          <div id="store__link" className="links__underline">
+            Shirts
+          </div>
         </Link>
         <Link to={"/category/pants"}>
-          <div id="store__link">Pants</div>
+          <div id="store__link" className="links__underline">
+            Pants
+          </div>
         </Link>
         <Link to={"/category/shoes"}>
-          <div id="store__link">Shoes</div>
+          <div id="store__link" className="links__underline">
+            Shoes
+          </div>
         </Link>
         <Link to={"/category/technology"}>
-          <div id="store__link">More</div>
+          <div id="store__link" className="links__underline">
+            More
+          </div>
         </Link>
         <Link to="/sale">
-          <div id="links__sale">Sale</div>
+          <div id="links__sale" className="links__underline">
+            Sale
+          </div>
         </Link>
       </div>
     </div>
